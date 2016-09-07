2c0216616f997153619e67365a3162183bd114ec
JoeGun <joshgun83@yahoo.com>
2016-09-07 07:06:56 -0700

    Add files via upload
    BlockingTemplateWindowView and BlockingTemplateWindowViewModel

---
+++ BlockingTemplateWindowView.xaml.cs
@@ -0,0 +1,95 @@
+﻿using System.Windows;
+using System;
+using Microsoft.Practices.Prism.Regions;
+using SynerMed.SHARE.Common.ViewModels.InteractionRequests;
+
+namespace SynerMed.SHARE.Common.Views.InteractionRequests
+{
+    public partial class BlockingTemplateWindowView
+    {
+        public BlockingTemplateWindowView()
+        {
+            InitializeComponent();
+            Visibility = Visibility.Hidden;
+            Loaded += OnUserControlLoaded;
+        }
+
+        #region Fields
+        private bool _isParentEnabled = true;
+        #endregion
+
+        #region Dependency Properties
+
+        public static readonly DependencyProperty IsDisplayedProperty =
+            DependencyProperty.Register(@"IsDisplayed", typeof (bool), typeof (BlockingTemplateWindowView),
+                new UIPropertyMetadata(false, IsDisplayedChangedCallback));
+
+        public static readonly DependencyProperty OverlayOnTopOfProperty =
+            DependencyProperty.Register(@"OverlayOnTopOf", typeof (UIElement), typeof (BlockingTemplateWindowView),
+                new UIPropertyMetadata(null));
+        #endregion
+
+        #region Properties
+        private BlockingTemplateWindowViewModel viewModel => DataContext as BlockingTemplateWindowViewModel;
+
+        public UIElement OverlayOnTopOf
+        {
+            get { return (UIElement)GetValue(OverlayOnTopOfProperty); }
+            set { SetValue(OverlayOnTopOfProperty, value); }
+        }
+
+        public bool IsDisplayed
+        {
+            get { return (bool)GetValue(IsDisplayedProperty); }
+            set { SetValue(IsDisplayedProperty, value); }
+        }
+        #endregion
+
+        #region Methods
+
+        private void OnUserControlLoaded(object sender, RoutedEventArgs e)
+        {
+            viewModel.OnBlockingTemplateClosed += OnViewModelBlockingTemplateClosed;
+            RegionManager.SetRegionName(BlockingTemplateContentControl, viewModel.RegionName);
+            RegionManager.SetRegionManager(BlockingTemplateContentControl, viewModel.RegionManager);
+        }
+
+        private void OnViewModelBlockingTemplateClosed(object sender, EventArgs e)
+        {
+            viewModel.OnBlockingTemplateClosed -= OnViewModelBlockingTemplateClosed;
+            RegionManager.SetRegionManager(BlockingTemplateContentControl, null);
+        }
+
+        public static void IsDisplayedChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            if ((bool)e.NewValue)
+            {
+                var dlg = (BlockingTemplateWindowView)d;
+                dlg.EnableView();
+            }
+            else
+            {
+                var dlg = (BlockingTemplateWindowView)d;
+                dlg.DisableView();
+            }
+        }
+
+        public void EnableView()
+        {
+            var expressionOverlayParent = GetBindingExpression(OverlayOnTopOfProperty);
+            expressionOverlayParent?.UpdateTarget();
+            if (OverlayOnTopOf == null) throw new InvalidOperationException("Can't get the UI Element where the window should reside.");
+            Visibility = Visibility.Visible;
+            _isParentEnabled = OverlayOnTopOf.IsEnabled;
+            OverlayOnTopOf.IsEnabled = false;
+        }
+
+        private void DisableView()
+        {
+            Visibility = Visibility.Hidden;
+            OverlayOnTopOf.IsEnabled = _isParentEnabled;
+        }
+
+        #endregion
+    }
+}
---
+++ BlockingTemplateWindowViewModel.cs
@@ -0,0 +1,100 @@
+﻿using SynerMed.SHARE.Framework.Libraries.EventHandlers;
+using SynerMed.SHARE.Framework.Libraries.ViewModels;
+using System;
+using System.Windows.Controls;
+
+namespace SynerMed.SHARE.Common.ViewModels.InteractionRequests
+{
+    public class BlockingTemplateWindowViewModel : ViewModelBase
+    {
+        public BlockingTemplateWindowViewModel()
+        {
+            RegionName = $@"RegionName_{Guid.NewGuid()}";
+        }
+
+        #region Properties
+
+        private UserControl _userControl;
+
+        private readonly WeakEvent<EventHandler> _onBlockingTemplateClosed = new WeakEvent<EventHandler>();
+
+        public event EventHandler OnBlockingTemplateClosed
+        {
+            add { _onBlockingTemplateClosed.RegisterHandler(value); }
+            remove { _onBlockingTemplateClosed.UnregisterHandler(value); }
+        }
+
+        private string _windowHeader;
+        public string WindowHeader
+        {
+            get { return _windowHeader; }
+            set
+            {
+                _windowHeader = value;
+                OnPropertyChanged(@"WindowHeader");
+            }
+        }
+
+        private string _regionName;
+        public string RegionName
+        {
+            get { return _regionName; }
+            set
+            {
+                _regionName = value;
+                OnPropertyChanged(@"RegionName");
+            }
+        }
+
+        private bool _isDisplayed;
+        public bool IsDisplayed
+        {
+            get { return _isDisplayed; }
+            set
+            {
+                _isDisplayed = value;
+                OnPropertyChanged(@"IsDisplayed");
+            }
+        }
+
+        #endregion
+
+        #region Methods
+
+        //public virtual void RegisterViewIntoBlockingTemplateWindowViewModel(UserControl userControl)
+        //{
+        //    RegionManager.RegisterViewWithRegion(RegionName, userControl.GetType());
+        //}
+
+        //public virtual void EnableView(string viewTitle)
+        //{
+        //    WindowHeader = viewTitle;
+        //    IsDisplayed = true;
+        //}
+
+        public virtual void EnableView(UserControl userControl, string viewTitle)
+        {
+            IsDisplayed = true;
+            WindowHeader = viewTitle;
+            var desiredRegion = RegionManager.Regions[RegionName];
+            if (desiredRegion == null) return;
+            _userControl = userControl;
+            if (!desiredRegion.Views.Contains(_userControl)) desiredRegion.Add(userControl);
+        }
+
+        public virtual void DisableView()
+        {
+            IsDisplayed = false;
+            var desiredRegion = RegionManager.Regions[RegionName];
+            if (desiredRegion == null) return;
+            if (desiredRegion.Views.Contains(_userControl)) desiredRegion.Remove(_userControl);
+        }
+
+        public void Close()
+        {
+            _onBlockingTemplateClosed.Invoke(null, null);
+        }
+
+        #endregion
+    }
+}
